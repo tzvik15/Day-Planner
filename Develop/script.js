f2f3356946d0a938ef27fc08329b4037543bcb1a
tzvik15 <tzvik15@yahoo.com>
2019-10-10 13:02:29 -0600

    updated html structure, added id's to textarea fields, created click listeners that store inputed text to unique local storage cells

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 //global variables
-var time = 9;
+
 
 
 //a function that displays and updates the time dynamically (the function call is built into the setInterval command)
@@ -8,24 +8,67 @@ var update = function() {
 }
 setInterval(update, 1000);
 
-
-
-
-//a function that creates calendar table rows and collumns
-
-var addHour = function() {
-    var newRow = $("<tr>");
-    newRow.attr("class", "newRow");
-    var newTh = $("<th>");
-    newTh.attr("scope", "row");
-    newTh.attr("class", "th");
-    $(".newRow").append(newTh);
-    var newTd = $("<td>");
-    newTd.attr("class", "td");
-    $(".newRow").append(newTd);
-    $(".th").text(time + "-" + (time+1));
-    $(".table").append(newRow);
-    console.log(time);
-    time++;
-}
-
+//a set of click events tied to the individual buttons that save the text in the textarea to local storage
+$("#cell1").on("click", function(event){
+    var text = (this).parentElement.childNodes[0].value;
+    var saved = JSON.parse(localStorage.getItem("memo1")) || [];
+    saved.splice(0);
+    saved.push({text});
+    localStorage.setItem("memo1", JSON.stringify(saved));
+})
+$("#cell2").on("click", function(event){
+    var text = (this).parentElement.childNodes[0].value;
+    var saved = JSON.parse(localStorage.getItem("memo2")) || [];
+    saved.splice(0);
+    saved.push({text});
+    localStorage.setItem("memo2", JSON.stringify(saved));
+})
+$("#cell3").on("click", function(event){
+    var text = (this).parentElement.childNodes[0].value;
+    var saved = JSON.parse(localStorage.getItem("memo3")) || [];
+    saved.splice(0);
+    saved.push({text});
+    localStorage.setItem("memo3", JSON.stringify(saved));
+})
+$("#cell4").on("click", function(event){
+    var text = (this).parentElement.childNodes[0].value;
+    var saved = JSON.parse(localStorage.getItem("memo4")) || [];
+    saved.splice(0);
+    saved.push({text});
+    localStorage.setItem("memo4", JSON.stringify(saved));
+})
+$("#cell5").on("click", function(event){
+    var text = (this).parentElement.childNodes[0].value;
+    var saved = JSON.parse(localStorage.getItem("memo5")) || [];
+    saved.splice(0);
+    saved.push({text});
+    localStorage.setItem("memo5", JSON.stringify(saved));
+})
+$("#cell6").on("click", function(event){
+    var text = (this).parentElement.childNodes[0].value;
+    var saved = JSON.parse(localStorage.getItem("memo6")) || [];
+    saved.splice(0);
+    saved.push({text});
+    localStorage.setItem("memo6", JSON.stringify(saved));
+})
+$("#cell7").on("click", function(event){
+    var text = (this).parentElement.childNodes[0].value;
+    var saved = JSON.parse(localStorage.getItem("memo7")) || [];
+    saved.splice(0);
+    saved.push({text});
+    localStorage.setItem("memo7", JSON.stringify(saved));
+})
+$("#cell8").on("click", function(event){
+    var text = (this).parentElement.childNodes[0].value;
+    var saved = JSON.parse(localStorage.getItem("memo8")) || [];
+    saved.splice(0);
+    saved.push({text});
+    localStorage.setItem("memo8", JSON.stringify(saved));
+})
+$("#cell9").on("click", function(event){
+    var text = (this).parentElement.childNodes[0].value;
+    var saved = JSON.parse(localStorage.getItem("memo9")) || [];
+    saved.splice(0);
+    saved.push({text});
+    localStorage.setItem("memo9", JSON.stringify(saved));
+})
